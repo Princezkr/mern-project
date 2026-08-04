b039c2c20117d1cb9d50decb7104dee815b7573d
Prince Kumar Sah <sahprince2707>
2026-08-04 10:14:31 +0530

    I have chanage the Home.jsx

--- mern-project/src/components/Navbar.jsx
+++ mern-project/src/components/Navbar.jsx
@@ -2,15 +2,15 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav style={{ padding: "15px", background: "#333", color: "#fff" }}>
+    <nav style={{ padding: "15px", background:"gray", color: "#fff" }}>
       <h2>College</h2>
-      
+    
       <div>
         <Link to="/" style={{ margin: "10px", color: "#fff" }}>Home</Link>
         <Link to="/about" style={{ margin: "10px", color: "#fff" }}>About</Link>
         <Link to="/courses" style={{ margin: "10px", color: "#fff" }}>Courses</Link>
       </div>
-    </nav>
+     </nav>
   );
 }
 
